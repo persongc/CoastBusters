a1939a43c333a2564e5f742e22447dc6d3ed7e63
ahavstad8 <ahavstad@calpoly.edu>
2018-12-02 22:25:04 -0800

    all tests

---
+++ src/test/UTest10.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UTest10 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/UnitTests3.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ UTest5.class, UTest6.class })
+public class UnitTests3 {
+
+}
