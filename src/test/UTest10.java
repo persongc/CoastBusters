6192fb52589dbd34fd410556cf4559389a49d62d
ahavstad8 <ahavstad@calpoly.edu>
2018-12-04 17:52:18 -0800

    Add files via upload

--- src/test/UTest10.java
+++ src/test/UTest10.java
@@ -4,11 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import logic.Hours;
+
 public class UTest10 {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Hours hr = new Hours("10", "11", 12);
+		assertEquals(12, hr.getTime(), 0);
 	}
 
 }
--- src/test/UTest7.java
+++ src/test/UTest7.java
@@ -4,11 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import logic.HikeInfo;
+
 public class UTest7 {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testName() {
+		HikeInfo h = new HikeInfo("hike1", true, false);
+		assertEquals("hike1", h.getName());
 	}
 
 }
